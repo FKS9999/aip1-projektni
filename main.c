ad12bccf416c4d51935326094136e365a1213ec3
FranK <fran.krtolica2@gmail.com>
2025-06-09 21:54:26 +0000

    Napravio logiku za kretanje

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
     // učitavanje prvog sloja labirinta
     char labirintSloj1[15][32] = {
         "#############################",
-        "E        #   #              #",
-        "#   # # #   # # #   # # #   #",
+        "E       #   #               #",
+        "#   #####   #####   #####   #",
         "#                   #       #",
-        "#   #   ##### # # # # # #   #",
+        "#   #####################   #",
         "#   #   #               #   #",
-        "# # #   #   # # # # #       #",
-        "#       #   #           #   #",
-        "# # #   # # #   # # # # #   #",
+        "#####   #   #########       #",
+        "#       #   #           #####",
+        "#####   # # #   #########   #",
         "#       #   #   #   #       #",
-        "# # #####       #   # # #   #",
+        "# #######       #   #####   #",
         "#       #   #   #       #   #",
-        "#    ## # # #   #   # # #   #",
+        "#    ########   #   #####   #",
         "#                            ",
         "############################>"};
 
@@ -38,16 +40,58 @@ int main()
         "                           P#",
         "                             "};
 
-    // Ispis oba sloja jedan na drugom
-    for (int i = 0; i < 15; i++)
+    int pozicijaX = 27;
+    int pozicijaY = 13;
+
+    while (1)
     {
-        for (int j = 0; j < 31; j++)
+        system("clear");
+        // Ispis oba sloja jedan na drugom
+        for (int i = 0; i < 15; i++)
         {
-            if (labirintSloj2[i][j] != ' ')
-                putchar(labirintSloj2[i][j]);
-            else
-                putchar(labirintSloj1[i][j]);
+            for (int j = 0; j < 31; j++)
+            {
+                if (labirintSloj2[i][j] != ' ')
+                    putchar(labirintSloj2[i][j]);
+                else
+                    putchar(labirintSloj1[i][j]);
+            }
+            putchar('\n');
         }
-        putchar('\n');
+
+        char input;
+        scanf(" %c", &input);
+        int novaX = pozicijaX;
+        int novaY = pozicijaY;
+
+        if (input == 'w' || input == 'W')
+        {
+            novaY--;
+        }
+        else if (input == 's' || input == 'S')
+        {
+            novaY++;
+        }
+        else if (input == 'a' || input == 'A')
+        {
+            novaX--;
+        }
+        else if (input == 'd' || input == 'D')
+        {
+            novaX++;
+        }
+
+        if (labirintSloj1[novaY][novaX] != '#')
+        {
+            labirintSloj2[pozicijaY][pozicijaX] = ' ';
+            pozicijaX = novaX;
+            pozicijaY = novaY;
+            labirintSloj2[pozicijaY][pozicijaX] = 'P';
+        }
+        if (labirintSloj1[novaY][novaX] == 'E')
+        {
+            printf("Kraj labirinta, izašo si iz labirinta.");
+            break;
+        }
     }
 }
